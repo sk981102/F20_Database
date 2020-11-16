4c062eb9476236ec7f152410ef167fff1cc9625f
Shaun Kim <shaunkim@Shauns-MacBook-Pro.local>
2020-11-16 21:18:52 +0900

    landing page created

--- trial/pages/views.py
+++ trial/pages/views.py
@@ -8,7 +8,7 @@ def home_view(request, *args, **kwargs):
 
 
 def submitter_landing_view(request, *args, **kwargs):
-    return render(request, "list.html", {})
+    return render(request, "submitter_landing.html", {})
 
 
 def rater_landing_view(request, *args, **kwargs):
---
+++ trial/rater/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url
+from rater import views
+
+urlpatterns = [
+    #url(r'^signup/$', core_views.signup, name='signup'),
+]
