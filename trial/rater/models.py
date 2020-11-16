f890f175ea3bca8512b3b9cb8c4da40d6aa7f734
Shaun Kim <shaunkim@Shauns-MacBook-Pro.local>
2020-11-16 22:30:47 +0900

    fixed rater

--- trial/rater/models.py
+++ trial/rater/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Rater(models.Model):
-    user_id = models.OneToOneField('accounts.UserProfile', models.DO_NOTHING, primary_key=True)
+    user_id = models.ForeignKey('accounts.UserProfile', models.DO_NOTHING, primary_key=True)
 
     class Meta:
         #managed = False
