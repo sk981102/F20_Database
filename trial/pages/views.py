5942e2c62e69123bb62734168cc9e5744af95b8a
Shaun Kim <sk981102@gmail.com>
2020-11-22 14:35:12 +0900

    submitter score

--- trial/pages/views.py
+++ trial/pages/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from submitter.models import Submitter
+from django.shortcuts import get_object_or_404
 
 
 # Create your views here.
@@ -7,12 +9,11 @@ def home_view(request, *args, **kwargs):
     return render(request, "home.html", {})
 
 def admin_landing_view(request, *args, **kwargs):
-    return render(request, "admin_landing.html", {''})
-
+    return render(request, "admin_landing.html", {})
 
 def submitter_landing_view(request, *args, **kwargs):
-    return render(request, "submitter_landing.html", {})
-
+    submitter = get_object_or_404(Submitter, pk=request.user.user_id)
+    return render(request, "submitter_landing.html", {"submitter": submitter})
 
 def rater_landing_view(request, *args, **kwargs):
     return render(request, "rater_landing.html", {})
