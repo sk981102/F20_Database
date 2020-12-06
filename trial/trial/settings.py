6785b64e257cfb31f13d0da15f600eda4029b345
minjoo <keenee@github.com>
2020-12-06 21:00:52 +0900

    템플릿 수정

--- trial/trial/settings.py
+++ trial/trial/settings.py
@@ -102,12 +102,12 @@ DATABASES = {
 }
 
 # default sqlite
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+##DATABASES = {
+##     'default': {
+##         'ENGINE': 'django.db.backends.sqlite3',
+##         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+##     }
+##}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
