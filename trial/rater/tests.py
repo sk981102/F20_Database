6559f355ae438ee664c041e0f8e5c62fafad2ec2
dailyavenger <65271296+dailyavenger@users.noreply.github.com>
2022-07-07 23:45:30 +0900

    df

--- trial/rater/tests.py
+++ trial/rater/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from sys import stdin
 
-# Create your tests here.
+N, M=stdin.readline().split()
+N=int(N)
+M=int(M)
+
+b=[]
+b=list(map(int, stdin.readline().split()))
+
+def binary(array,target,start, end):
+    sum=0
+    mid=(start+end)//2
+    for j in range(N):
+        if(b[j]>mid):
+            sum=sum+b[j]-mid
+
+    if(start>mid):
+        return None
+    elif (start==mid):
+        return mid
+    elif (target>sum):
+        binary(array,  target,start, mid-1)
+    else:
+        binary(array, target, mid+1, end)
+
+print(binary(b, M, 0, max(b)))
